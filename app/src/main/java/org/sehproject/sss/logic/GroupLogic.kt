6a3c279783739ec41e32eca7737f18eca81748d0
shion0202 <wjdtndyd34510@gmail.com>
2021-11-11 18:25:52 +0900

    add logic

---
+++ app/src/main/java/org/sehproject/sss/logic/GroupLogic.kt
@@ -0,0 +1,70 @@
+package org.sehproject.sss.logic
+
+import org.sehproject.sss.viewmodel.GroupViewModel
+
+class GroupLogic(val groupViewModel: GroupViewModel) {
+    fun onCreateGroupClick()
+    {
+
+    }
+
+    fun onCreateGroupCompleteClick()
+    {
+
+    }
+
+    fun onDeleteGroupClick()
+    {
+
+    }
+
+    fun onDeleteGroupCompleteClick()
+    {
+
+    }
+
+    fun onEditGroupClick()
+    {
+
+    }
+
+    fun onEditGroupCompleteClick()
+    {
+
+    }
+
+    fun onAcceptGroupClick(groupid: Int)
+    {
+
+    }
+
+    fun onRefuseGroupClick(groupid: Int)
+    {
+
+    }
+
+    fun onInviteGroupClick()
+    {
+
+    }
+
+    fun onInviteGroupDoneClick()
+    {
+
+    }
+
+    fun onViewGroupDetailClick()
+    {
+
+    }
+
+    fun getGroupList()
+    {
+
+    }
+
+    fun getGroup(groupid: Int)
+    {
+
+    }
+}
---
+++ app/src/main/java/org/sehproject/sss/logic/UserLogic.kt
@@ -0,0 +1,15 @@
+package org.sehproject.sss.logic
+
+import org.sehproject.sss.viewmodel.UserViewModel
+
+class UserLogic(val userViewModel: UserViewModel) {
+    fun onLoginClick()
+    {
+
+    }
+
+    fun onRegisterClick()
+    {
+
+    }
+}
