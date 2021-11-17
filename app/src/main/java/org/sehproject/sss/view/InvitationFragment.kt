a76d94fd2ec2389deead1c225c9f2470b069c2b9
shion0202 <wjdtndyd34510@gmail.com>
2021-11-17 14:29:34 +0900

    delete todo in fragment

--- app/src/main/java/org/sehproject/sss/view/InvitationFragment.kt
+++ app/src/main/java/org/sehproject/sss/view/InvitationFragment.kt
@@ -8,29 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import org.sehproject.sss.viewmodel.FriendViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AcceptInvitationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InvitationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/org/sehproject/sss/view/PlanEditFragment.kt
+++ app/src/main/java/org/sehproject/sss/view/PlanEditFragment.kt
@@ -14,29 +14,7 @@ import org.sehproject.sss.databinding.FragmentPlanDetailBinding
 import org.sehproject.sss.databinding.FragmentPlanEditBinding
 import org.sehproject.sss.viewmodel.PlanViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PlanEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PlanEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
