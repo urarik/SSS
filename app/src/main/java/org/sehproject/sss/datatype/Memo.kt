51acece9c271129d577f8c60925ca76cc109b5bd
박세훈 <psh3253@naver.com>
2021-11-27 20:17:12 +0900

    버그 픽스

--- app/src/main/java/org/sehproject/sss/datatype/Memo.kt
+++ app/src/main/java/org/sehproject/sss/datatype/Memo.kt
@@ -1,3 +1,3 @@
 package org.sehproject.sss.datatype
 
-data class Memo(var memo: String = "", var pid: Int = 0, var writer: User? = null)
+data class Memo(var memo: String = "", var pid: Int = 0, var writer: String? = "")
